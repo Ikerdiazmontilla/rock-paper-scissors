98c0597b2cc02970ecd82d3d29322ed056482aa9
Iker <143113054+Ikerdiazmontilla@users.noreply.github.com>
2023-10-16 19:36:35 -0500

    Finish game to 5 rounds. Have to fix the finish
    I put the all the function calls into the event listener
    so that the event listeners wouldn't stack one on top of the other.

--- script.js
+++ script.js
@@ -1,67 +1,81 @@
-
-
-        function rockPaperScissors(){
+let userScore = 0
+let computerScore = 0
+let result = ""
+const divResult = document.querySelector("#results")
+const userScoreSpan = document.querySelector("#userScore")
+const computerScoreSpan = document.querySelector("#computerScore")
+userScoreSpan.textContent = userScore
+computerScoreSpan.textContent = computerScore
         
-
-            // Make a function called getComputerChoice that finds the computer choice:
-            function getComputerChoice(){
+function rockPaperScissors(){
+        
+function getComputerChoice(){
                 
 
-                // Gets one random value from "Rock""Paper""Scissors" and stores it in a variable called computerChoice.
-                    const options = ["paper","rock","scissors"];
-                    const random = Math.floor(Math.random()*options.length);
-                    const computerChoice = options[random];
-                    return computerChoice
-            }
+    const options = ["paper","rock","scissors"];
+    const random = Math.floor(Math.random()*options.length);
+    const computerChoice = options[random];
+    return computerChoice
+    }
+
+    const computerChoice = getComputerChoice();
+    
+
+    
+    divResult.textContent = `You chose ${userChoice}  |  Computer chose ${computerChoice}`
 
 
-
-            const computerChoice = getComputerChoice();
-
-            //Cancel the process
-            if (userChoice === "" || userChoice === null){
-                return;
-            }
-
-            // print `Your choice was ${userChoice}`
-            // print `Computer choice was ${computerChoice}`
-            console.log(`You chose ${userChoice}`)
-            console.log(`Computer chose ${computerChoice}`)
-
-
-
-
-            // Create a function called getResult that gets the winner:
-            function getResult(userChoice,computerChoice){
-                // if userChoice equals paper and computerChoice equals rock OR  user === rock AND computer === scissors OR user === scissors and computer === paper: return "user wins"
-                if (userChoice === "paper" && computerChoice === "rock" || userChoice === "rock" && computerChoice === "scissors" || userChoice === "scissors" && computerChoice === "paper"){
-                    return "You won!!!"
-                }
-                // else if userChoice === computerChoice: return "You are in a tie!"
-                else if (userChoice === computerChoice){
-                    return "Its a tie!!!"
-                }
-                // return "computer wins"
-                return "Computer won :("
-
-            }
-            result = getResult(userChoice,computerChoice)
-
-            // print(getResult)
-            console.log(result)
+    function getResult(userChoice,computerChoice){
+        if (userChoice === "paper" && computerChoice === "rock" || userChoice === "rock" && computerChoice === "scissors" || userChoice === "scissors" && computerChoice === "paper"){
+            return "You won!!!"
         }
+        else if (userChoice === computerChoice){
+            return "Its a tie!!!"
+        }
+        return "Computer won :("
 
-        let userChoice = ""
-        let buttons = document.querySelectorAll("button")
-        buttons.forEach(
-            (button) => {
-                button.addEventListener("click", 
-                () => {
-                    userChoice = button.className
-                    rockPaperScissors()
-            })
-            })
+    }
 
+
+    result = getResult(userChoice,computerChoice)
+    divResult.innerHTML += `<br>${result}`
+}
+
+
+
+
+let userChoice = ""
+
+let buttons = document.querySelectorAll("button")
+
+buttons.forEach(
+    (button) => {
+        button.addEventListener("click", 
+        () => {
+            userChoice = button.className
+            rockPaperScissors()
         
-       
-        
+
+
+    if (result === "You won!!!"){
+        userScore = userScore + 1
+    }
+    else if (result === "Computer won :("){
+        computerScore = computerScore + 1
+    }
+
+    userScoreSpan.textContent = userScore
+    computerScoreSpan.textContent = computerScore
+    scoreboard = document.querySelector("#scoreboard")
+
+    if (computerScore === 5){
+        divResult.textContent = `I'm sorry my G, computer won the war`
+    }
+
+    else if(userScore === 5){
+        scoreboard.textContent = `You smashed the computer baby!!!`
+    }
+
+})
+})
+    
